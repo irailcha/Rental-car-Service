222abb6dab8cf0ed35fcfb2c4d293b19585f8b7d
irailcha <karpusenko.irina.13@gmail.com>
2024-08-21 11:13:01 +0300

    created pages and components for rental car web-site

---
+++ cloudinaryConfig.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: 'dgdokikcv',
+  api_key: '348823922152171',
+  api_secret: 'qHga0JPesqLRdOZOCwG_FTOwQbA'
+});
+
+export default cloudinary;
