f4725345b27762dbd2eabf6fcb7d4892d110fe25
irailcha <115711713+irailcha@users.noreply.github.com>
2024-08-21 13:12:55 +0000

    Deploying to gh-pages from @ irailcha/rental-car-service@98d7633ba719b7658f0a51db84c772c9d3d49d37 🚀

--- cloudinaryConfig.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: 'dgdokikcv',
-  api_key: '348823922152171',
-  api_secret: 'qHga0JPesqLRdOZOCwG_FTOwQbA'
-});
-
-export default cloudinary;
--- src/components/Layout/Layout.style.js
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-
-export const MainContainer = styled.div`
-  position: relative;
-  padding-top: 50px;
-`;
